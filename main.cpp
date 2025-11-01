5a644a8c8960393ca7a3378e2f3799c3a52aefad
johnny200138 <jthai7050@sdsu.edu>
2025-10-31 22:53:00 -0700

    c1

--- main.cpp
+++ main.cpp
@@ -91,12 +91,20 @@ int createLeafNodes(int freq[]) {
 int buildEncodingTree(int nextFree) {
     // TODO:
     // 1. Create a MinHeap object.
+    MinHeap heap;
     // 2. Push all leaf node indices into the heap.
+    heap.push(nextFree, weightArr);
     // 3. While the heap size is greater than 1:
     //    - Pop two smallest nodes
     //    - Create a new parent node with combined weight
     //    - Set left/right pointers
     //    - Push new parent index back into the heap
+    while (heap.size > 1) {
+       int leftChild = heap.pop(weightArr);
+       int rightChild = heap.pop(weightArr);
+
+
+    }
     // 4. Return the index of the last remaining node (root)
     return -1; // placeholder
 }
